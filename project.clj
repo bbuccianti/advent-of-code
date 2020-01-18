209cde715456d033f10ecfce27896823452547e1
Benjamin Buccianti <benjamin@buccianti.dev>
2020-01-18 20:14:17 -0300

    AOC2015/2: avoid reflection

--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
   :description "Advent of Code"
   :license "MIT"
   :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.match "0.3.0"]
                  [criterium "0.4.5"]]
   :target-path "target/%s"
   :global-vars {*warn-on-reflection* true
                 *unchecked-math* :warn-on-boxed}
   :profiles {:uberjar {:aot :all}
-             :dev {:plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]
-                   :test-refresh {:quiet true
-                                  :changes-only true}}})
+             :kaocha {:dependencies [[lambdaisland/kaocha "0.0-581"]]}}
+  :aliases {"kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]})
--- src/advent_of_code/2015/2.clj
+++ src/advent_of_code/2015/2.clj
@@ -8,21 +8,23 @@
         l (first splited)
         w (second splited)
         h (last splited)]
-    {:length (Integer. l)
-     :width (Integer. w)
-     :height (Integer. h)}))
+    {:length (Integer/parseInt l)
+     :width (Integer/parseInt w)
+     :height (Integer/parseInt h)}))
 
 (defn to-wrapping [input-map]
-  (let [lw (* (:length input-map) (:width input-map))
-        wh (* (:width input-map) (:height input-map))
-        hl (* (:height input-map) (:length input-map))]
+  (let [lw (* ^int (:length input-map) ^int (:width input-map))
+        wh (* ^int (:width input-map) ^int (:height input-map))
+        hl (* ^int (:height input-map) ^int (:length input-map))]
     (+ (* lw 2) (* wh 2) (* hl 2) (min lw wh hl))))
 
 (defn to-ribbon [input-map]
   (let [sorted (sort (vals input-map))
-        ff (* 2 (first sorted))
-        ss (* 2 (second sorted))
-        bow (* (:length input-map) (:width input-map) (:height input-map))]
+        ff (* 2 ^int (first sorted))
+        ss (* 2 ^int (second sorted))
+        bow (* ^int (:length input-map)
+               ^int (:width input-map)
+               ^int (:height input-map))]
     (+ ff ss bow)))
 
 (defn calc-from-file [f]
